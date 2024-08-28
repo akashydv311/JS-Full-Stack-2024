8137442092bb3774da634ff6c30878f69efd2dc2
akashydv311 <akashydv311@gmail.com>
2024-08-28 07:31:13 +0530

    add dom to repo

--- week-0/js/DOM/cssIndex.js
+++ week-0/js/DOM/cssIndex.js
@@ -7,9 +7,34 @@ title1.style.color = 'red';
 
 
 const listiteam = document.querySelector('.list');
-// console.log(fontsize);
+// console.log(listiteam);
 // fontsize.style.fontsize = '5em';
 
-for (let i = 0; i < listiteam.length; i++) {
-    listiteam[i].style.fontSize = '5em';
-}
+// for (let i = 0; i < listiteam; i++) {
+//     listiteam[i].style.fontSize = '20px';
+// }
+
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+ul.append(li);
+li.innerHTML = "Game of throns (sharukh khan)";
+
+// let text = li.innerText;
+// console.log(text);
+
+const li01 = document.querySelectorAll('li');
+// console.log(li01[5].innerText);
+
+
+const firstTitle = document.querySelector('#head1');
+// console.log(firstTitle);
+// console.log(firstTitle.innerHTML);
+// console.log(firstTitle.textContent);
+// console.log(firstTitle.innerText);
+
+
+
+
+
+
