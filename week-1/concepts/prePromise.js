5fc27b3140da79eb41b81deaf43985663dea16ab
akashydv311 <akashydv311@gmail.com>
2024-08-31 23:30:32 +0530

    add prepromise

---
+++ week-1/concepts/prePromise.js
@@ -0,0 +1,44 @@
+
+// whey promises where introduces
+
+/*
+
+function getFunction1() {
+    console.log("get function - 1");
+}
+
+function getFunction2() {
+    console.log("get function - 2");
+}
+
+function getFunction3() {
+    console.log("get function - 3");
+}
+
+
+setTimeout(getFunction1, 1000);
+setTimeout(getFunction2, 2000);
+setTimeout(getFunction3, 3000);
+
+*/
+
+
+
+
+// Issuue with the callBack hell
+// Callbacks is a soluction of callback hells
+
+
+setTimeout(function getFunction1() {
+    console.log("get function - 1");
+
+    setTimeout(function getFunction2() {
+        console.log("get function - 2");
+
+        setTimeout(function getFunction3() {
+            console.log("get function - 3");
+        }, 6000);
+
+    }, 4000);
+
+}, 2000)
