543bed54eb6da60cfe0e4eb042a8071c5e8912ab
akashydv311 <akashydv311@gmail.com>
2024-09-01 17:42:19 +0530

    add promises code

---
+++ week-1/concepts/promises.js
@@ -0,0 +1,57 @@
+
+/*
+
+// dammy synchornous fun
+function dammy() {
+    console.log("Nothing!");
+}
+
+// promises
+
+function promiseBody(resolve) {
+    setTimeout(resolve, 1000);
+}
+
+
+function runFunction() {
+    let asnwer = new Promise(promiseBody);
+    return asnwer;
+}
+
+let result = runFunction();
+console.log(result);
+
+// setInterval(() => { console.log("Hello") }, 1000);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+function goingToBuyPen() {
+    console.log("Got the pen");
+}
+
+function promiseBody(resolve) {
+    setTimeout(resolve, 5000)
+}
+
+function goBuyPenFromMarket() {
+    let thing = new Promise(promiseBody);
+    return thing;
+}
+
+let result = goBuyPenFromMarket();
+console.log(result);
+
+result.then(goingToBuyPen);
+
